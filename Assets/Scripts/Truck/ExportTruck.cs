9d2d2bbed04e04d655c95ad2ef14858c8dc08342
anonymuspro <dexsper@mail.ru>
2022-03-20 16:21:31 +0600

    Export Truck

---
+++ Assets/Scripts/Truck/ExportTruck.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ExportTruck : TruckBase
+{
+    public override void AddPackage(Package package)
+    {
+        var cell = Cells[_packages.Count];
+
+        base.AddPackage(package);
+
+        StartCoroutine(package.MoveTo(cell.transform, cell, 1f));
+
+        cell.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (_targetDoor != null && _reachDoor == false)
+        {
+            float distance = Vector3.Distance(transform.position, _targetDoor.transform.position);
+
+            if (distance <= _stopDistance)
+            {
+                _reachDoor = true;
+                return;
+            }
+
+            MoveTo(_targetDoor.transform.position);
+        }
+        else if (_reachDoor && Equipped && Release == false)
+        {
+            float distance = Vector3.Distance(transform.position, _startPos);
+
+            if (distance <= _stopDistance)
+            {
+                Release = true;
+                return;
+            }
+
+            MoveTo(_startPos);
+        }
+    }
+}
+
